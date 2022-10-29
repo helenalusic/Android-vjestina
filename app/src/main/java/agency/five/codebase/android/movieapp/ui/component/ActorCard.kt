60f0cb3520924962d02e2f0795f6d8cb13c47f70
lusichelena <lusichelena@gmail.com>
2022-10-29 23:44:36 +0200

    Homework1

--- app/src/main/java/agency/five/codebase/android/movieapp/ui/component/ActorCard.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/ui/component/ActorCard.kt
@@ -1,22 +1,26 @@
 package agency.five.codebase.android.movieapp.ui.component
 
 import agency.five.codebase.android.movieapp.mock.MoviesMock
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
-import androidx.compose.material3.Text
+import agency.five.codebase.android.movieapp.model.Actor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import coil.request.ImageRequest
+
 
 data class ActorCardViewState(
     val imageUrl: String,
@@ -25,10 +29,60 @@ data class ActorCardViewState(
 )
 @Composable
 fun ActorCard(
-    actorCardViewState: ActorCardViewState,
+    actorCardViewState: Actor,
     modifier: Modifier = Modifier,
 ) {
-
+    Card(
+        modifier = Modifier
+            .padding(3.dp)
+            .size(width = 140.dp, height = 200.dp),
+        shape = RoundedCornerShape(13)
+    ) {
+        Box {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(actorCardViewState.imageUrl)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .height(200.dp)
+            )
+        }
+        Box {
+            Column {
+                Box (
+                    modifier = Modifier
+                        .height(150.dp)
+                )
+                Text(
+                    text = actorCardViewState.name,
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    fontFamily = FontFamily.Serif,
+                    color = Color.White,
+                    modifier = Modifier
+                        .background(Color.Black.copy(0.7f))
+                        .padding(start = 10.dp, top = 5.dp)
+                        .width(140.dp)
+                        .height(20.dp)
+                )
+                Text(
+                    text = actorCardViewState.character,
+                    fontSize = 9.sp,
+                    fontStyle = FontStyle.Italic,
+                    fontFamily = FontFamily.Serif,
+                    color = Color.White,
+                    modifier = Modifier
+                        .background(Color.Black.copy(0.7f))
+                        .padding(start = 10.dp)
+                        .width(140.dp)
+                        .height(25.dp)
+                )
+            }
+        }
+    }
 
 }
 @Preview
@@ -37,5 +91,5 @@ private fun ActorCardPreview() {
     val actor = MoviesMock.getActor()
     val viewState = ActorCardViewState(actor.name, actor.character, actor.imageUrl.toString())
 
-    ActorCard(actorCardViewState)
+    ActorCard(actorCardViewState =MoviesMock.getActor())
 }
---
+++ app/src/main/java/agency/five/codebase/android/movieapp/ui/component/FavoriteButton.kt
@@ -0,0 +1,49 @@
+package agency.five.codebase.android.movieapp.ui.component
+
+import agency.five.codebase.android.movieapp.R
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+
+
+@Composable
+fun FavoriteButton(
+    modifier: Modifier = Modifier
+) {
+
+    var isFavorite by rememberSaveable { mutableStateOf(true) }
+
+    Image(
+        painter = painterResource(
+            id = if (isFavorite) R.drawable.favorite_heart_filled else R.drawable.favorite_heart_outlined
+        ),
+        contentDescription = null,
+
+        modifier = Modifier
+            .clickable { isFavorite = !isFavorite }
+            .size(32.dp)
+            .background(color = Color.Blue, shape = CircleShape)
+            .padding(5.dp),
+        contentScale = ContentScale.Crop
+    )
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FavoriteButtonPreview() {
+    FavoriteButton()
+}
